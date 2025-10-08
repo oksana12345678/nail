bef344fd89c58b9d9d88ec16040b0b65615d1e8b
Oksana Tanashchuk <144157970+oksana12345678@users.noreply.github.com>
2025-10-08 17:11:58 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -49,8 +49,8 @@ export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
 }
 
-export default async function RootLayout(props: LayoutProps) {
-  const { children, params } = props;
+export default async function RootLayout({children, params}: LayoutProps) {
+
   const { locale } = await params;
 
   const { resources } = await initTranslations(locale, i18nNamespaces);
