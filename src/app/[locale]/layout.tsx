ea19fbb7de78bf1a86ce3ced9c08124496d9f083
Oksana Tanashchuk <144157970+oksana12345678@users.noreply.github.com>
2025-10-08 17:36:44 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -32,7 +32,7 @@ const mainMetadataDict: Record<Language, { description: string }> = {
 
 export async function generateMetadata({
   params,
-}
+}:params{locale:string }
 ): Promise<Metadata> {
   const { locale } = params;
 
