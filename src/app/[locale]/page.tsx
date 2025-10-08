291c24eb2e8ca7dd35fc0540d90e4e22e86cf3bf
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-10-08 15:16:53 +0200

    deploy fix

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
 import Home from '@/pages/Home/Home';
+import { Language } from '@/shared/types';
 
 export default async function MainPage({
   params,
 }: {
-  params: { locale: string };
+  params: { locale: Language };
 }) {
-  const {locale} = await params;
+  const { locale } = await params;
   return (
     <main className="font-sans  flex flex-col justify-center items-center">
       <Home locale={locale} />
