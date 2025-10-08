70f629260ae61bcfc58cc9321416a78a86e6bec8
Oksana Tanashchuk <144157970+oksana12345678@users.noreply.github.com>
2025-10-08 17:17:01 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,9 +3,9 @@ import { Language } from '@/shared/types';
 
 export default async function MainPage({
   params,
-}: {
-  params: { locale: Language };
-}) {
+}:  
+LayoutProps
+ ) {
   const { locale } = await params;
   return (
     <main className="font-sans  flex flex-col justify-center items-center">
